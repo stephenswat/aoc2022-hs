344423e02129ff5447eae4b5fcb30c5e6971a736
Stephen Nicholas Swatman <stephen@v25.nl>
2022-12-01 11:25:09 +0100

    Add solutions to day 1

--- src/Problems/Day01.hs
+++ src/Problems/Day01.hs
@@ -1,6 +1,14 @@
 module Problems.Day01 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Data.List (sort)
+import Data.List.Split (splitWhen)
+import Common.Solution (Day)
+
+groups :: String -> [[Int]]
+groups = map (map read) . splitWhen null . lines
 
 solution :: Day
-solution = (notImplemented, notImplemented)
+solution = (
+        show . maximum . map sum . groups,
+        show . sum . take 3 . reverse . sort . map sum . groups
+    )

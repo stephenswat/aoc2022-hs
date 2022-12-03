cc55b7ebb68b98197c9cee92549b5219dff4d705
Stephen Nicholas Swatman <stephen@v25.nl>
2022-12-03 11:59:05 +0100

    Add solution for day 3

--- src/Problems/Day03.hs
+++ src/Problems/Day03.hs
@@ -1,6 +1,25 @@
 module Problems.Day03 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Data.Char (ord)
+import Data.Set (toList, intersection, fromList)
+import Data.Tuple.Extra (both)
+import Data.List.Split (chunksOf)
+
+import Common.Solution (Day)
+
+common :: String -> [Char]
+common s = toList . uncurry intersection . both fromList . splitAt ((length s) `div` 2) $ s
+
+value :: Char -> Int
+value c
+    | o >= ord 'a' && o <= ord 'z' = o - (ord 'a') + 1
+    | o >= ord 'A' && o <= ord 'Z' = o - (ord 'A') + 27
+    | otherwise = error "Invalid character!"
+    where o = ord c
 
 solution :: Day
-solution = (notImplemented, notImplemented)
+solution = (
+        f (map common),
+        f (map (toList . foldl1 intersection . map fromList) . chunksOf 3)
+    )
+    where f g = show . sum . map value . concat . g . lines

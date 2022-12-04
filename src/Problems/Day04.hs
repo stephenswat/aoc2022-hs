3b44275a850ec72d94cf03da9040491262602401
Stephen Nicholas Swatman <stephen@v25.nl>
2022-12-04 14:40:11 +0100

    Add solution to day 4

--- src/Problems/Day04.hs
+++ src/Problems/Day04.hs
@@ -1,6 +1,29 @@
 module Problems.Day04 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Data.Set (Set, fromList, isSubsetOf, intersection)
+import Text.Parsec (Parsec, sepEndBy, newline)
+import Text.Parsec.Char (char)
+
+import Common.Solution (Day)
+import Common.Parse (aocParse, integer)
+
+input :: Parsec String () [(Set Integer, Set Integer)]
+input = sepEndBy ranges newline
+    where
+        range = do
+            l <- integer;
+            _ <- char '-';
+            r <- integer;
+            return (fromList [l..r])
+        ranges = do
+            l <- range;
+            _ <- char ',';
+            r <- range;
+            return (l, r)
 
 solution :: Day
-solution = (notImplemented, notImplemented)
+solution = (f p1, f p2)
+    where
+        f p = show . length . filter p . aocParse input
+        p1 (a, b) = a `isSubsetOf` b || b `isSubsetOf` a
+        p2 (a, b) = not . null $ intersection a b

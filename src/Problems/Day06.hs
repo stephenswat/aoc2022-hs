0719c6f86c3d0fca69bf0766556671ceef4814f7
Stephen Nicholas Swatman <stephen@v25.nl>
2022-12-06 10:33:43 +0100

    Add solution to day 6

--- src/Problems/Day06.hs
+++ src/Problems/Day06.hs
@@ -1,6 +1,15 @@
 module Problems.Day06 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Data.List (nub)
+
+import Common.Solution (Day)
+
+segmentsOf :: Int -> [a] -> [[a]]
+segmentsOf n xs
+    | length xs >= n = (take n xs):(segmentsOf n (tail xs))
+    | otherwise = []
 
 solution :: Day
-solution = (notImplemented, notImplemented)
+solution = (f 4, f 14)
+    where
+        f n = show . fst . head . filter (\(_, x) -> x == nub x) . zip [n..] . segmentsOf n

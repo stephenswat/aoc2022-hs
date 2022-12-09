90701580c4dd1b2ecd86763ae20d6fff59cfe975
Stephen Nicholas Swatman <stephen@v25.nl>
2022-12-09 13:11:50 +0100

    Add solution to day 8

--- src/Problems/Day08.hs
+++ src/Problems/Day08.hs
@@ -1,6 +1,34 @@
 module Problems.Day08 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Data.Map (size, filterWithKey, findWithDefault, mapWithKey, toList)
+
+import Common.Solution (Day)
+import Common.Geometry (Grid2D, readGrid2D)
+
+solve1 :: Grid2D Integer -> Int
+solve1 g = size . filterWithKey f $ g
+    where
+        f (x, y) v = or [
+                and [(findWithDefault (-1) (x, y') g) < v | y' <- [0..(y - 1)]],
+                and [(findWithDefault (-1) (x, y') g) < v | y' <- [(y + 1)..100]],
+                and [(findWithDefault (-1) (x', y) g) < v | x' <- [0..(x - 1)]],
+                and [(findWithDefault (-1) (x', y) g) < v | x' <- [(x + 1)..100]]
+            ]
+
+countTrue :: [Bool] -> Int
+countTrue [] = 0
+countTrue (False:_) = 0
+countTrue (True:xs) = 1 + (countTrue xs)
+
+solve2 :: Grid2D Integer -> Int
+solve2 g = maximum . map snd . toList . mapWithKey f $ g
+    where
+        f (x, y) v = (
+                (1 + countTrue [(findWithDefault 10 (x, y') g) < v | y' <- reverse [1..(y - 1)]]) *
+                (1 + countTrue [(findWithDefault 10 (x, y') g) < v | y' <- [(y + 1)..97]]) *
+                (1 + countTrue [(findWithDefault 10 (x', y) g) < v | x' <- reverse [1..(x - 1)]]) *
+                (1 + countTrue [(findWithDefault 10 (x', y) g) < v | x' <- [(x + 1)..97]])
+            )
 
 solution :: Day
-solution = (notImplemented, notImplemented)
+solution = (show . solve1 . readGrid2D, show . solve2 . readGrid2D)

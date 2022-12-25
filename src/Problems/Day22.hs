11f687027fd9a97c227ed1b81cf2cbea4056e5aa
Stephen Nicholas Swatman <stephen@v25.nl>
2022-12-25 21:26:02 +0100

    Add solution to day 22

--- src/Problems/Day22.hs
+++ src/Problems/Day22.hs
@@ -1,15 +1,16 @@
 module Problems.Day22 (solution) where
 
+import Data.Tuple.Extra (both)
 import Data.List (uncons)
 import Data.Map (fromList, toList, findWithDefault)
 import Text.Parsec ((<|>), sepEndBy1, many, newline, char)
 
-import Common.Solution (Day, notImplemented)
+import Common.Solution (Day)
 import Common.Geometry (Grid2D, Point2D)
 import Common.Parse (AocInput, aocParse, integer)
 import Common.Cardinal (Direction (..), Rotation (..), rotate, translate)
 
-data Tile = Open | Wall | Void deriving (Eq, Show)
+data Tile = Open | Wall deriving (Eq, Show)
 
 data Move = Forward Integer | Turn Rotation deriving (Eq, Show)
 
@@ -51,16 +52,44 @@ transition1 (0, 3) North = ((0, 2), North)
 transition1 (0, 3) West  = ((0, 3), West)
 transition1 (0, 3) South = ((0, 2), South)
 transition1 (0, 3) East  = ((0, 3), East)
+transition1 _      _     = error "Invalid transition!"
+
+transition2 :: Transition
+transition2 (1, 0) North = ((0, 3), East)
+transition2 (1, 0) West  = ((0, 2), East)
+transition2 (1, 0) South = ((1, 1), South)
+transition2 (1, 0) East  = ((2, 0), East)
+transition2 (2, 0) North = ((0, 3), North)
+transition2 (2, 0) West  = ((1, 0), West)
+transition2 (2, 0) South = ((1, 1), West)
+transition2 (2, 0) East  = ((1, 2), West)
+transition2 (1, 1) North = ((1, 0), North)
+transition2 (1, 1) West  = ((0, 2), South)
+transition2 (1, 1) South = ((1, 2), South)
+transition2 (1, 1) East  = ((2, 0), North)
+transition2 (0, 2) North = ((1, 1), East)
+transition2 (0, 2) West  = ((1, 0), East)
+transition2 (0, 2) South = ((0, 3), South)
+transition2 (0, 2) East  = ((1, 2), East)
+transition2 (1, 2) North = ((1, 1), North)
+transition2 (1, 2) West  = ((0, 2), West)
+transition2 (1, 2) South = ((0, 3), West)
+transition2 (1, 2) East  = ((2, 0), West)
+transition2 (0, 3) North = ((0, 2), North)
+transition2 (0, 3) West  = ((1, 0), South)
+transition2 (0, 3) South = ((2, 0), South)
+transition2 (0, 3) East  = ((1, 2), North)
+transition2 _      _     = error "Invalid transition!"
 
 input :: AocInput () (Grid2D Tile, [Move])
 input = do
     rs <- sepEndBy1 row newline;
     ms <- many move;
-    return (fromList [((x, y), t) | (y, r) <- zip [0..] rs, (x, t) <- zip [0..] r], ms)
+    return (fromList [((x, y), t) | (y, r) <- zip [0..] rs, (x, Just t) <- zip [0..] r], ms)
     where
-        tile = (char '.' >> return Open) <|>
-               (char '#' >> return Wall) <|>
-               (char ' ' >> return Void)
+        tile = (char '.' >> return (Just Open)) <|>
+               (char '#' >> return (Just Wall)) <|>
+               (char ' ' >> return (Nothing))
         move = (char 'L' >> return (Turn RotateLeft)) <|>
                (char 'R' >> return (Turn RotateRight)) <|>
                (integer >>= return . Forward)
@@ -76,39 +105,55 @@ initGame g m = GameState
         minY = minimum [y | ((_, y), t) <- toList g, t == Open]
         minX = minimum [x | ((x, y), t) <- toList g, t == Open, y == minY]
 
-moveForward :: Grid2D Tile -> Direction -> Point2D -> Point2D
-moveForward g d p
-    | t == Open = m
-    | t == Wall = p
+rotateGrid :: Direction -> Direction -> Point2D -> Point2D
+rotateGrid d1 d2 (x, y)
+    | d1 == d2 = (x, y)
+    | otherwise = rotateGrid (rotate RotateLeft d1) d2 (y, -x + 49)
+
+transition :: Transition -> (Integer, Integer) -> Direction -> Point2D -> (Point2D, Direction)
+transition t s d p = ((50 * ntx + ndx, 50 * nty + ndy), nd)
+    where
+        ((ntx, nty), nd) = t s d
+        p' = both (`mod` 50) p
+        (ndx, ndy) = rotateGrid d nd p'
+
+moveForward :: Transition -> Grid2D Tile -> Direction -> Point2D -> (Point2D, Direction)
+moveForward f g d p
+    | t == Open = (np, nd)
+    | t == Wall = (p, d)
     | otherwise = error "Invalid!"
     where
         n = translate d p
-        b = rotate RotateLeft . rotate RotateLeft $ d
-        m = if findWithDefault Void n g == Void
-            then translate d . head . filter (\x -> findWithDefault Void x g == Void) . iterate (translate b) $ p
-            else n
-        t = findWithDefault Void m g
+        tf = side p
+        tt = side n
+        (np, nd) = if tf /= tt
+            then transition f tf d n
+            else (n, d)
+        t = findWithDefault Wall np g
 
-playMove :: Grid2D Tile -> GameState -> GameState
-playMove g s
+playMove :: Transition -> Grid2D Tile -> GameState -> GameState
+playMove f g s
     | Nothing <- r = s
     | Just (Turn d, t) <- r = s { facing=rotate d (facing s), moves=t }
     | Just (Forward 0, t) <- r = s { moves=t }
-    | Just (Forward n, t) <- r = s { position=moveForward g (facing s) (position s), moves=if n <= 1 then t else Forward (n - 1):t }
+    | Just (Forward n, t) <- r = s { position=np, facing=nd, moves=if n <= 1 then t else Forward (n - 1):t }
     where
         r = uncons . moves $ s
-
-scoreFacing :: Direction -> Integer
-scoreFacing North = 3
-scoreFacing West  = 2
-scoreFacing South = 1
-scoreFacing East  = 0
+        (np, nd) = moveForward f g (facing s) (position s)
 
 scoreState :: GameState -> Integer
 scoreState s = 1000 * ((+1) . snd . position $ s) + 4 * ((+1) . fst . position $ s) + scoreFacing (facing s)
+    where
+        scoreFacing North = 3
+        scoreFacing West  = 2
+        scoreFacing South = 1
+        scoreFacing East  = 0
 
-play :: Grid2D Tile -> [Move] -> GameState
-play g m = until (null . moves) (playMove g) . initGame g $ m
+play :: Transition -> Grid2D Tile -> [Move] -> GameState
+play f g m = until (null . moves) (playMove f g) . initGame g $ m
 
 solution :: Day
-solution = (show . scoreState . uncurry play . aocParse input (), notImplemented)
+solution = (
+        show . scoreState . uncurry (play transition1) . aocParse input (),
+        show . scoreState . uncurry (play transition2) . aocParse input ()
+    )
